69a18d860f4ca9698d6ad520c19f8e2cb16c2563
Thomas Sileghem <tsileghem@neworbit.co.uk>
2015-12-08 22:35:48 +0000

    style: replace flow by the software name in test

--- app/templates/test.js
+++ app/templates/test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import binCheck from 'bin-check';
-import flow from './';
+import <%= softwareName %> from './';
 
 test('returns path to binary and verify that it is working', async t => {
-	t.true(await binCheck(flow, ['--version']));
+	t.true(await binCheck(<%= softwareName %>, ['--version']));
 });
